6a7b41b9e57e97131cd99e439a6e282902e23295
LaTorya <walker76w@gmail.com>
2024-02-11 14:50:06 -0800

    Completed steps for edit_trips

--- trips/urls.py
+++ trips/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from trips.views import create_trip, trip_list, show_trip
+from trips.views import create_trip, trip_list, show_trip, edit_trip
 
 
 urlpatterns = [
     path("create/", create_trip, name="create_trip"),
     path("trips/", trip_list, name="trip_list"),
     path("trips/<int:trip_id>/", show_trip, name="show_trip"),
+    path("edit/<int:id>/", edit_trip, name="edit_trip")
 ]
--- trips/views.py
+++ trips/views.py
@@ -32,3 +32,19 @@ def trip_list(request):
     trips = Trip.objects.all()
     context = {"trips": trips}
     return render(request, "trips/list.html", context)
+
+@login_required
+def edit_trip(request, id):
+    trip = get_object_or_404(Trip, id=id)
+    if request.method == 'POST':
+        form = TripForm(request.POST, instance=trip)
+        if form.is_valid():
+            form.save()
+            return redirect('show_trip', id=id)
+    else:
+        form = TripForm(instance=trip)
+    context = {
+        'form': form,
+        'trip': trip,
+    }
+    return render(request,'trips/edit.html', context)
