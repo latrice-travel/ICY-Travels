a34168ffe1096818609efb7489bdc1b9e8e256a3
LaTorya <walker76w@gmail.com>
2024-02-11 16:59:04 -0800

    Completed Login steps

---
+++ students/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class StudentLoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(max_length=150, widget=forms.PasswordInput)
+
+
+class StudentSignUpForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(max_length=150, widget=forms.PasswordInput)
+    password_confirmation = forms.CharField(max_length=150,widget=forms.PasswordInput)
--- students/views.py
+++ students/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from students.forms import StudentLoginForm, StudentSignUpForm
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = StudentLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = StudentLoginForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'students/login.html', context)
